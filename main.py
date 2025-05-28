91263cdae1e6572ceb2261194814051e38a169e7
Tukachevsky1776 <etucker5253@gmail.com>
2025-05-27 21:49:43 -0500

    added flexibility(finished!)

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
+import sys
 from stats import get_word_count
 from stats import get_char_count
 from stats import sort_char_count
+
+if len(sys.argv)<2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -10,13 +15,12 @@ def get_book_text(file_path):
     return book_content
 
 def main():
-    frankenstein_text = get_book_text("books/frankenstein.txt")
-    print(frankenstein_text)
-    return frankenstein_text
+    book_text = get_book_text(sys.argv[1])
+    return book_text
 
 main_var = main()
 print(f"""============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------
 Found {len(get_word_count(main_var))} total words
 --------- Character Count -------""")
