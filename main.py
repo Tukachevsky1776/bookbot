343feb34827f2f686cd24ff82a44e9f127c263c2
Tukachevsky1776 <etucker5253@gmail.com>
2025-05-27 21:33:03 -0500

    made it pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from stats import get_word_count
 from stats import get_char_count
+from stats import sort_char_count
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -14,5 +15,12 @@ def main():
     return frankenstein_text
 
 main()
-get_word_count(main())
-get_char_count(main())
+main_var = main()
+print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {len(get_word_count(main_var))} total words
+--------- Character Count -------""")
+for entry in sort_char_count(get_char_count(main_var)):
+    print(f"{entry['char']}: {entry['num']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_word_count(book_string):
     words = book_string.split()
-    print (f"{len(words)} words found in the document")
+    return words
 
 def get_char_count(book_string):
     chars_table = {}
@@ -10,5 +10,19 @@ def get_char_count(book_string):
                     chars_table[char] += 1
         else:
             chars_table[char] = 1
-    print(chars_table)
     return chars_table
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_char_count(dict):
+    dict_list = []
+    for entry in dict:
+        if entry.isalpha():
+            char = {"char": entry}
+            num = {"num": dict[entry]}
+            dict_list.append({**char, **num})
+        else:
+            pass
+    dict_list.sort(reverse = True, key = sort_on)
+    return dict_list
